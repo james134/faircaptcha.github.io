534ae9f1c6327b43121cfb9b8477b53f7e23f75b
Danny Domso <danny@Dannys-MacBook-Pro.local>
2020-09-04 19:28:13 +0100

    adding express module

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
-//var path = require('path');
+var path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/register', function(request, response){
     console.log("reg");
@@ -15,11 +19,7 @@ app.post('/register', function(request, response){
     }
 });
 
-/*
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-*/
+
 
 app.listen(process.env.PORT || 8080, function () {
     console.log('Node app is live!');
