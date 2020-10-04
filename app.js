d75b3b9ffb8f77272ca1cb49719094e0168b87ed
Harry Winner KF <kamdemharry@gmail.com>
2020-10-04 12:13:19 +0300

    improving server to handle register requests

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var express = require('express');
+const express = require('express');
+const crypto = require("crypto");
 var app = express();
-var path = require('path');
+const path = require('path');
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.PG_URI,
+})
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -10,15 +15,34 @@ app.get('/', function (req, res) {
 });
 
 app.post('/register', function(request, response){
+    //POST keys: email,endpoint,signals
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    let kv = "";
-    for (const key in request.body) {  
-       kv += (key+" : "+request.body[key]+"\n")
-    }
-    response.end(kv);
-});
+    const email_regex = /\S+@\S+\.\S+/;
 
+    if ("email" in request.body
+        && email_regex.test(request.body.email)
+        && "signals" in request.body) {
+        let now = new Date().getTime();
+        const text = 'INSERT INTO users(api_key, secret, signals, email) VALUES($1, $2, $3, $4) RETURNING *'
+        const values = [
+            crypto.createHash('md5').update(now + email).digest('hex'),
+            crypto.createHash('sha256').update(now + email).digest('hex'),
+            "'{\""+ request.body.signals.split(',').join('","') +"\"}'",
+            request.body.email.trim().toLowerCase()
+        ]
+        pool.query(text, values, (err, res) => {
+            if (err) {
+                console.log(err.stack)
+                response.end(err.stack)
+            } else {
+                console.log(res.rows[0])
+                response.end(res.rows[0])
+            }
+            //pool.end()
+        })
+    } else response.end("");
+});
 
 
 app.listen(process.env.PORT || 8080, function () {
