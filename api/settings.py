7b5422920df187caf4f2a3ca3b55317b98fcafef
james134 <tallajames601@gmail.com>
2021-04-29 20:17:59 +0100

    bug statics files

--- api/settings.py
+++ api/settings.py
@@ -157,5 +157,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
-
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
