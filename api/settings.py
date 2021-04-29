2284136f0c3cf5c3124148f3598c530bce76b85f
james134 <tallajames601@gmail.com>
2021-04-29 17:27:10 +0100

    bug statics files

--- api/settings.py
+++ api/settings.py
@@ -155,7 +155,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/js/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 django_heroku.settings(locals())
